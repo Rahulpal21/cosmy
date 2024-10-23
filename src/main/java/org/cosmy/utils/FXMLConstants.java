48c8befeb758806613104788a1a419dde62b336e
Rahul Pal <rahulpal21@gmail.com>
2024-10-23 05:55:18 -0400

    #28; fixed fxml loading issue with itemtab

--- src/main/java/org/cosmy/utils/FXMLConstants.java
+++ src/main/java/org/cosmy/utils/FXMLConstants.java
@@ -2,5 +2,5 @@ package org.cosmy.utils;
 
 public interface FXMLConstants {
     String ACCOUNTS_PANE_FXML = "accountsPane";
-    String ITEMS_TAB_FXML = "itemsTab2";
+    String ITEMS_TAB_FXML = "itemTab2";
 }
