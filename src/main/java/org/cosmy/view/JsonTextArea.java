5494ece4025025952b1b45488987f2ec05b85323
Rahul Pal <rahulpal21@gmail.com>
2025-01-04 20:11:21 -0500

    #29 added basic indentation

--- src/main/java/org/cosmy/view/JsonTextArea.java
+++ src/main/java/org/cosmy/view/JsonTextArea.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cosmy.controllers.itemtab.JsonPrinterFactory;
-import org.cosmy.utils.StyleConstants;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 
@@ -12,29 +11,29 @@ import static org.cosmy.utils.StyleConstants.*;
 
 public class JsonTextArea extends CodeArea {
     private final ObjectMapper jsonPrinter = JsonPrinterFactory.getJsonPrinter();
+    private IndentationHelper indentationHelper;
 
     public JsonTextArea() {
         super();
         setParagraphGraphicFactory(LineNumberFactory.get(this));
         autosize();
+        indentationHelper = new IndentationHelper();
     }
 
-    public void setText(JsonNode text) {
-        //TODO styled implementation
+    public void setText(JsonNode jsonNode) {
+        this.clear();
+        appendJsonStartParenthesis();
+        jsonNode.fields().forEachRemaining(nodeEntry -> {
+            appendAttributeKey(nodeEntry.getKey());
+            appendAttributeValue(nodeEntry.getValue());
+            appendNewline();
+        });
+        appendJsonEndParenthesis();
     }
 
     public void setText(String text) throws JsonProcessingException {
-        //TODO styled implementation
-        this.clear();
         try {
-            JsonNode jsonNode = jsonPrinter.readTree(text);
-            appendJsonStartParenthesis();
-            jsonNode.fields().forEachRemaining(nodeEntry -> {
-                appendAttributeKey(nodeEntry.getKey());
-                appendAttributeValue(nodeEntry.getValue());
-                appendNewline();
-            });
-            appendJsonEndParenthesis();
+            setText(jsonPrinter.readTree(text));
         } catch (JsonProcessingException e) {
             throw e;
         }
@@ -46,10 +45,12 @@ public class JsonTextArea extends CodeArea {
 
     private void appendJsonEndParenthesis() {
         this.append(END_PARENTHESIS, STYLE_KEY);
+        indentationHelper.decrementIndent();
     }
 
     private void appendJsonStartParenthesis() {
         this.append(START_PARENTHESIS, STYLE_KEY);
+        indentationHelper.incrementIndent();
     }
 
     private void appendAttributeValue(JsonNode value) {
@@ -57,7 +58,7 @@ public class JsonTextArea extends CodeArea {
     }
 
     private void appendAttributeKey(String key) {
-        this.append(SINGLE_QUOTE + key + SINGLE_QUOTE + COLON, STYLE_KEY);
+        this.append(indentationHelper.indent() + SINGLE_QUOTE + key + SINGLE_QUOTE + COLON, STYLE_KEY);
     }
 
     public String getText() {
