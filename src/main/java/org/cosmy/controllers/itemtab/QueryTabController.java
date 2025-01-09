4206b091c50b2e05ba348fe153f120e510c64744
Rahul Pal <rahulpal21@gmail.com>
2025-01-09 16:22:35 -0500

    #57 implemented database and collection context for querytab

--- src/main/java/org/cosmy/controllers/itemtab/QueryTabController.java
+++ src/main/java/org/cosmy/controllers/itemtab/QueryTabController.java
@@ -5,19 +5,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import org.cosmy.context.ConnectionsContainer;
 import org.cosmy.context.IObservableModelRegistry;
 import org.cosmy.context.ObservableModelRegistryImpl;
 import org.cosmy.model.CosmosContainer;
+import org.cosmy.model.CosmosDatabase;
 import org.cosmy.model.ObservableModelKey;
 import org.cosmy.spec.IController;
+import org.cosmy.view.QueryPaneContextBar;
 import org.w3c.dom.Text;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class QueryTabController implements IController {
     private final CosmosContainer container;
     private final String tabName;
 
     @FXML
-    private HBox queryPaneToolbarLeft;
+    private QueryPaneContextBar queryPaneToolbarLeft;
     @FXML
     private HBox queryPaneToolbarRight;
     @FXML
@@ -42,6 +49,13 @@ public class QueryTabController implements IController {
     public void initialize() {
         IObservableModelRegistry modelRegistry = ObservableModelRegistryImpl.getInstance();
         ObservableList<Tab> tabs = (ObservableList<Tab>) modelRegistry.lookup(ObservableModelKey.TABS);
+        queryPaneToolbarLeft.initialize(extractDatabaseList(container), container);
+    }
+
+    private Set<CosmosDatabase> extractDatabaseList(CosmosContainer container) {
+        Set<CosmosDatabase> dbs = new HashSet<>();
+        dbs.add(container.getParent());
+        return dbs;
     }
 
 }
