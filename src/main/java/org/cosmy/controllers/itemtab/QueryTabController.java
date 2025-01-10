9f943eb94fd2d74bcf4722f895a353d57e13c6a7
Rahul Pal <rahulpal21@gmail.com>
2025-01-09 20:14:15 -0500

    #57 basic query execution and results display implemented

--- src/main/java/org/cosmy/controllers/itemtab/QueryTabController.java
+++ src/main/java/org/cosmy/controllers/itemtab/QueryTabController.java
@@ -1,7 +1,11 @@
 package org.cosmy.controllers.itemtab;
 
+import com.azure.cosmos.models.FeedResponse;
+import com.azure.cosmos.util.CosmosPagedFlux;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
@@ -10,18 +14,19 @@ import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
-import org.cosmy.context.ConnectionsContainer;
 import org.cosmy.context.IObservableModelRegistry;
 import org.cosmy.context.ObservableModelRegistryImpl;
 import org.cosmy.model.CosmosContainer;
 import org.cosmy.model.CosmosDatabase;
 import org.cosmy.model.ObservableModelKey;
 import org.cosmy.spec.IController;
+import org.cosmy.utils.AppConstants;
+import org.cosmy.view.DialogPopup;
 import org.cosmy.view.QueryPaneContextBar;
-import org.w3c.dom.Text;
+import reactor.core.publisher.Flux;
 
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class QueryTabController implements IController {
@@ -41,9 +46,12 @@ public class QueryTabController implements IController {
     @FXML
     private TextArea resultsPane;
 
+    private final ObjectMapper jsonPrinter;
+
     public QueryTabController(CosmosContainer container) {
         this.container = container;
         this.tabName = generateTabName(container);
+        this.jsonPrinter = JsonPrinterFactory.getJsonPrinter();
     }
 
     private String generateTabName(CosmosContainer container) {
@@ -58,10 +66,36 @@ public class QueryTabController implements IController {
 
         KeyCombination keyCombination = new KeyCodeCombination(KeyCode.ENTER, KeyCombination.CONTROL_DOWN);
         queryPaneEditor.addEventHandler(KeyEvent.KEY_PRESSED, keyEvent -> {
-            if(keyCombination.match(keyEvent)){
-                System.out.println("CNTRL + ENTER  pressed");
+            if (keyCombination.match(keyEvent)) {
+                String query = extractRawQuery();
+                submitQuery(query);
             }
         });
+
+    }
+
+    private void submitQuery(String query) {
+        CosmosPagedFlux<Map> pagedFlux = container.getAsyncContainer().queryItems(query, Map.class);
+        Flux<FeedResponse<Map>> page = pagedFlux.byPage(AppConstants.RESULTS_PAGE_SIZE);
+        page.handle((mapFeedResponse, synchronousSink) -> {
+            mapFeedResponse.getElements().forEach(map -> {
+                Platform.runLater(() -> {
+                    try {
+                        resultsPane.appendText(jsonPrinter.writeValueAsString(map)+"\n");
+                    } catch (JsonProcessingException e) {
+                        System.out.println(e);
+                        //TODO log error on dialog or console
+                    }
+                });
+            });
+        }).doOnError(throwable -> {
+            new DialogPopup(throwable.getMessage());
+        }).subscribe();
+
+    }
+
+    private String extractRawQuery() {
+        return queryPaneEditor.getText();
     }
 
     private Set<CosmosDatabase> extractDatabaseList(CosmosContainer container) {
