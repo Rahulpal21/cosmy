388f996e7ae7ad7003024aee6bd742a8f6033b92
Rahul Pal <rahulpal21@gmail.com>
2025-01-25 13:08:38 -0500

    removed unused buttons from querypane

--- src/main/java/org/cosmy/view/QueryPaneContextBar.java
+++ src/main/java/org/cosmy/view/QueryPaneContextBar.java
@@ -13,25 +13,25 @@ import java.util.stream.Collectors;
 public class QueryPaneContextBar extends HBox {
     private static final String DATABASE_LABEL = "Database";
     private static final String COLLECTION_LABEl = "Collection";
-    private ComboBox<String> database;
-    private ComboBox<String> collection;
+//    private ComboBox<String> database;
+//    private ComboBox<String> collection;
 
     public QueryPaneContextBar() {
         super();
     }
 
     public void initialize(Set<CosmosDatabase> databases, CosmosContainer selectedContainer) {
-        this.getChildren().add(new Label(DATABASE_LABEL));
-        database = new ComboBox<>();
-        database.getItems().setAll(databases.stream().map(CosmosDatabase::getName).collect(Collectors.toSet()));
-        database.getSelectionModel().select(selectedContainer.getParent().getName());
-        this.getChildren().add(database);
+        this.getChildren().add(new Label(DATABASE_LABEL.concat(": ".concat(selectedContainer.getParent().getName()).concat(", "))));
+//        database = new ComboBox<>();
+//        database.getItems().setAll(databases.stream().map(CosmosDatabase::getName).collect(Collectors.toSet()));
+//        database.getSelectionModel().select(selectedContainer.getParent().getName());
+//        this.getChildren().add(database);
 
-        this.getChildren().add(new Label(COLLECTION_LABEl));
-        collection = new ComboBox<>();
-        collection.getItems().setAll(extractContainerList(selectedContainer.getParent()));
-        collection.getSelectionModel().select(selectedContainer.getName());
-        this.getChildren().add(collection);
+        this.getChildren().add(new Label(COLLECTION_LABEl.concat(": ".concat(selectedContainer.getName()))));
+//        collection = new ComboBox<>();
+//        collection.getItems().setAll(extractContainerList(selectedContainer.getParent()));
+//        collection.getSelectionModel().select(selectedContainer.getName());
+//        this.getChildren().add(collection);
     }
 
     private Set<String> extractContainerList(CosmosDatabase parent) {
